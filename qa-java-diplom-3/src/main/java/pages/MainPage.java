9e28ec52bff1b6c4c52855731e515813c5be306a
Petlevannyy-Sergey <spetlevannyy@gmail.com>
2025-11-05 15:41:21 +0300

    Тестирование переключения между вкладками

--- qa-java-diplom-3/src/main/java/pages/MainPage.java
+++ qa-java-diplom-3/src/main/java/pages/MainPage.java
@@ -13,6 +13,24 @@ public class MainPage {
     //Кнопка "Личный кабинет"
     private final By accountButton = By.xpath(".//p[text()='Личный Кабинет']");
 
+    //Кнопка "Булки"
+    private final By bunsButton = By.xpath(".//span[text()='Булки']");
+
+    //Кнопка "Соусы"
+    private final By saucesButton = By.xpath(".//span[text()='Соусы']");
+
+    //Кнопка "Начинки"
+    private final By fillingsButton = By.xpath(".//span[text()='Начинки']");
+
+    // Вкладка "Булки" активна
+    private final By bunsActiveTab = By.xpath(".//div[contains(@class, 'current') and contains(.//span, 'Булки')]");
+
+    // Вкладка "Соусы" активна
+    private final By saucesActiveTab = By.xpath(".//div[contains(@class, 'current') and contains(.//span, 'Соусы')]");
+
+    // Вкладка "Начинки" активна
+    private final By fillingsActiveTab = By.xpath(".//div[contains(@class, 'current') and contains(.//span, 'Начинки')]");
+
     public MainPage(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
@@ -23,5 +41,29 @@ public class MainPage {
 
     public void clickAccountButton() {
         webDriver.findElement(accountButton).click();
+    }
+
+    public void clickBunsButton(){
+        webDriver.findElement(bunsButton).click();
+    }
+
+    public void clickSaucesButton(){
+        webDriver.findElement(saucesButton).click();
+    }
+
+    public void clickFillingButton(){
+        webDriver.findElement(fillingsButton).click();
+    }
+
+    public boolean bunsTabIsActive() {
+        return webDriver.findElement(bunsActiveTab).isEnabled();
+    }
+
+    public boolean saucesTabIsActive() {
+        return webDriver.findElement(saucesActiveTab).isEnabled();
+    }
+
+    public boolean fillingsTabIsActive() {
+        return webDriver.findElement(fillingsActiveTab).isEnabled();
     }
 }
---
+++ qa-java-diplom-3/src/test/java/login/ConstructorTests.java
@@ -0,0 +1,66 @@
+package login;
+
+import config.WebConfig;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import pages.MainPage;
+
+public class ConstructorTests {
+    private WebDriver driver;
+    private MainPage mainPage;
+
+    @Before
+    public void setup() {
+        driver = WebConfig.runChromeDriver();
+        mainPage = new MainPage(driver);
+    }
+
+    @Test
+    @DisplayName("Переключение на вкладку 'Булки'")
+    public void SwitchToBunTabIsSuccess() {
+        // Arrange
+
+        // Act
+        driver.navigate().to(MainPage.url);
+        mainPage.clickFillingButton();
+        mainPage.clickBunsButton();
+
+        // Assert
+        Assert.assertTrue("Некорректное переключение на вкладку 'Булки'", mainPage.bunsTabIsActive());
+    }
+
+    @Test
+    @DisplayName("Переключение на вкладку 'Соусы'")
+    public void SwitchToSauceTabIsSuccess() {
+        // Arrange
+
+        // Act
+        driver.navigate().to(MainPage.url);
+        mainPage.clickSaucesButton();
+
+        // Assert
+        Assert.assertTrue("Некорректное переключение на вкладку 'Соусы'", mainPage.saucesTabIsActive());
+    }
+
+    @Test
+    @DisplayName("Переключение на вкладку 'Начинки'")
+    public void SwitchToFillingTabIsSuccess() {
+        // Arrange
+
+        // Act
+        driver.navigate().to(MainPage.url);
+        mainPage.clickFillingButton();
+
+        // Assert
+        Assert.assertTrue("Некорректное переключение на вкладку 'Начинки'", mainPage.fillingsTabIsActive());
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+}
